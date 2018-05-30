7ace756e8dc2edbf86a0e54e58e4bae0df4dcaae
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-05-30 17:26:59 -0500

    added column price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510183516) do
+ActiveRecord::Schema.define(version: 20180530222543) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150510183516) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image_url"
+    t.decimal  "price"
   end
 
 end

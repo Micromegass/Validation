da845e577e52c88a2641f1625b32a29e24a71e15
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-05-30 16:03:32 -0500

    added validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,10 @@
 #
 
 class Room < ActiveRecord::Base
+    validates :id, presence: true
+    validates :title, presence: true
+    validates :description, presence: true, length: { maximum: 500 }
+    validates :beds, presence: true, numericality: { only_integer: true }
+    validates :guests, presence: true, numericality: { only_integer: true }
+    validates :image_url, presence: true
 end
